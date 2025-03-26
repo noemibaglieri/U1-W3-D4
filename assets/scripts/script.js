39ab4eabce4a9cb376ed586bedee9dc20f537cf1
Noemi Baglieri <noemibaglieri@gmail.com>
2025-03-26 23:05:03 +0100

    completed homework

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,27 @@
 const form = document.querySelector("form");
 
-
 form.onsubmit = function (e) {
-    e.preventDefault();
-    const listOfTasks = document.getElementById("list-of-tasks");
-    const taskInput = document.getElementById("add-input")
-    const task = document.createElement("li");
-    const deleteTaskBtn = document.createElement("button")
-    task.classList.add("task");
+  e.preventDefault();
+  const listOfTasks = document.getElementById("list-of-tasks");
+  const taskInput = document.getElementById("add-input");
+  const task = document.createElement("li");
+  const deleteTaskBtn = document.createElement("button");
+  task.classList.add("task");
 
-    deleteTaskBtn.type = "button"
-    deleteTaskBtn.innerText = "delete"
-    
-    task.innerText = taskInput.value
-    listOfTasks.appendChild(task)
-    task.appendChild(deleteTaskBtn)
-    task.onclick = function (e) {
-        e.target.classList.toggle("completed-task")}
-        deleteTaskBtn.className = "delete-task-button"
-    
+  deleteTaskBtn.type = "button";
+  deleteTaskBtn.innerText = "❌";
 
-}
+  task.innerText = taskInput.value;
+  listOfTasks.appendChild(task);
 
+  task.appendChild(deleteTaskBtn);
+
+  task.onclick = function (e) {
+    e.target.classList.toggle("completed-task");
+  };
+  deleteTaskBtn.className = "delete-task-button";
+  deleteTaskBtn.onclick = function (e) {
+    e.target.closest(".task").remove();
+  };
+  taskInput.value = "";
+};
